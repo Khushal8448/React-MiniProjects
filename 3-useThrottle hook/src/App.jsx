009f1f6d704a6853b042e8b7e69f67e247d73108
Khushal Mali <khushalmali8448@gmail.com>
2024-06-17 18:34:16 +0530

    hook

--- 3-useThrottle hook/src/App.jsx
+++ 3-useThrottle hook/src/App.jsx
@@ -1,7 +1,36 @@
-import "./App.css";
+import { useEffect, useState } from "react";
+import useThrottle from "./hooks/use-thottle";
 
 function App() {
-  return <h1>Hi</h1>;
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  const handleResize = () => {
+    setWindowSize({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  };
+
+  const throttleHandleResize = useThrottle(handleResize, 500);
+
+  useEffect(() => {
+    window.addEventListener("resize", throttleHandleResize);
+
+    return () => {
+      window.removeEventListener("resize", throttleHandleResize);
+    };
+  }, []);
+
+  return (
+    <>
+      <h1>
+        Window Size: {windowSize.width} x {windowSize.height}
+      </h1>
+    </>
+  );
 }
 
 export default App;
